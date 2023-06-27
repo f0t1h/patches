c31c12417446ed7c08e9750ddddcdbe41f41a8a2
Fatih Karaoglanoglu <fatihkaraoglanoglu@gmail.com>
2023-06-27 14:34:33 -0700

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -1,36 +1,75 @@
 #include <patches.hpp>
 
 int main(){
-    using person = ns::patches< 
-        ns::field<"Age", int>,
-        ns::field<"Name", const char *>
-    >;
- 
-    person Y;
-    ns::getto<"Age">(Y) = 30;
-    ns::getto<"Name">(Y) = "Foti";
+ using pers = ns::patches <
+        ns::field<"name", string>,
+        ns::field<"age", int>
+        >;
     
-    person Z {21, "HOIII"};
-
-    vector<person> persons;
-    persons.emplace_back(12, "ME");
-
-    array<person, 2> onlar {{
-        {33, "o"},
-        {0, "biz"}
-    }};
-
-    using evlicift = ns::patches<
-        ns::field<"e1", person>,
-        ns::field<"e2", person>
+    using location = ns::patches <
+        ns::field<"country", string>,
+        ns::field<"province", string>
     >;
 
-    evlicift cif {
-        {30,"merve"},
-        {32,"mahmut"}
-    };
-    ns::getto<"e1","Age"> (cif) +=25;
-    cout << ns::serialize::tsv(ns::getto<"e2">(cif)) <<"\n";
+    using wine = ns::patches <
+        ns::field<"name", string>,
+        ns::field<"age", int>,
+        ns::field<"location", location>
+    >;
 
-    return ns::getto<"Age">(Y) + ns::getto<"Age">(Z);
-}
+    pers fatih {"fatih",42};
+
+    wine w1 {
+        "champagne",
+        33,
+        {"France", "Champagne"}
+    };
+
+    auto print_name = [] (const ns::field<"name",string> &f){
+        cout << f.name() << ":" << f.value;
+    };
+    auto print_age = [] (const ns::field<"age",int> &f){
+        cout << f.name() << ":" << f.value;
+    };
+    auto print_country = [] (const ns::field<"country", string> &f){
+        cout << f.name() << ":" << f.value;
+    };
+    auto print_province = [] (const ns::field<"province", string> &f){
+        cout << f.name() << ":" << f.value;
+    };
+    auto print_location = [&] (const ns::field<"location", location> &f){
+        cout << f.name() << ":(";
+        print_country(f.value);
+        cout << ", ";
+        print_province(f.value);
+        cout << ")";
+    };
+
+    print_name(fatih);
+    cout << "\n";
+    print_age(fatih);
+
+
+    cout << "\n";
+
+    print_name(w1);
+    cout << "\n";
+    print_age(w1);
+    cout << "\n";
+    print_location(w1);
+
+    using fam = ns::patches<
+        ns::field<"p1", pers>,
+        ns::field<"p2", pers>,
+        ns::field<"p3", pers>,
+        ns::field<"p4", pers>
+    >;
+
+    fam nice {
+        {"A",20},
+        {"B",21},
+        {"C",22},
+        {"D",23},
+    };
+    ns::getto<"p3","age">(nice)+=10;
+    return ns::getto<"p3","age">(nice);    
